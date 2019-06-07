abf5b06849591c0411dc755c15bf917758fc154b
isc-wre <rene.winkler@isc-ejpd.admin.ch>
2019-06-07 17:10:03 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SvgGaugeComponent } from './svg-gauge/svg-gauge.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SvgGaugeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      BrowserAnimationsModule,
+    MatSliderModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
